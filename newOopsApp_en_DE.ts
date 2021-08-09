9d01afff2421b260149c2e345b846d5ff770634d
cubeelement <cubeelement@protonmail.com>
2021-08-09 08:42:44 +0200

    Project initial configuration

---
+++ newOopsApp_en_DE.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="Build/newOopsApp_autogen/include/ui_mainwindow.h" line="50"/>
+        <location filename="newOopsApp_autogen/include/ui_mainwindow.h" line="50"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
