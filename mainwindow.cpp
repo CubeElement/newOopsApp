dea5f86b7535e3f2b0403685966a10288612c9ec
cubeelement <cubeelement@protonmail.com>
2021-08-31 12:23:38 +0200

    Dynamic signals/slots for the report data

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,15 +6,10 @@
 
 #include <QSpinBox>
 #include <QHBoxLayout>
-#include <QVBoxLayout>
 #include <QSpacerItem>
 #include <QSizePolicy>
-#include <QMap>
 #include <QDebug>
-#include <QHashIterator>
-#include <QGroupBox>
 #include <QObject>
-#include <stdexcept>
 #include <iterator>
 #include <QCompleter>
 #include <QMessageBox>
@@ -107,7 +102,6 @@ void MainWindow::createSelectorList(const std::set<std::string>& units_list)
 
 void MainWindow::on_button_option_proceed_clicked()
 {
-    this->clearReportData();
     if ( ui->radio_address->isChecked() )
     {
         ui->stackedWidget->setCurrentIndex(2);
@@ -129,11 +123,17 @@ void MainWindow::on_button_option_proceed_clicked()
 
 void MainWindow::receiveSelectionValues(QString name, int count)
 {
-    if ( ui->stackedWidget->currentIndex() == 2 )
+    QString count_str = QString::number(count);
+    if ( ui->stackedWidget->currentIndex() == 2 && count != 0)
     {
-        ui->page_2_list_newsp->addItem(name + " " + QString::number(count)
-                                       + " pcs.");
-    } else if ( ui->stackedWidget->currentIndex() == 3 )
+        ui->page_2_list_newsp->addItem(name + " " + count_str + " pcs.");
+        QObject::connect(this,
+                         &MainWindow::sendReportData,
+                         this,
+                         [=]() { MainWindow::showReport(
+                                name,
+                                count_str); });
+    } else if ( ui->stackedWidget->currentIndex() == 3  && count != 0)
     {
         QLabel* title = new QLabel();
         title->setText(name);
@@ -146,6 +146,12 @@ void MainWindow::receiveSelectionValues(QString name, int count)
             QCompleter* completer = new QCompleter(*addrlist, this);
             address->setCompleter(completer);
             ui->layout_newsp_addresses->addWidget(address);
+            QObject::connect(this,
+                             &MainWindow::sendReportData,
+                             this,
+                             [=]() { MainWindow::showReport(
+                                    name,
+                                    address->text()); });
         }
     }
 }
@@ -153,11 +159,7 @@ void MainWindow::receiveSelectionValues(QString name, int count)
 void MainWindow::sendReport()
 {
     ui->stackedWidget->setCurrentIndex(4);
-    this->showReport();
-}
 
-void MainWindow::showReport()
-{
     QLabel* shipping_address_annotation = new QLabel();
     shipping_address_annotation->setText(QString("Your request have been sent \n"));
     ui->vlayout_report->addWidget(shipping_address_annotation);
@@ -168,37 +170,21 @@ void MainWindow::showReport()
         courier_addr->setText(QString("Courier's address: ") + address_from_user);
         ui->vlayout_report->addWidget(courier_addr);
     }
-    std::map <std::string, int>::iterator it;
-    for ( it = this->m_ReportNewsp.begin(); it != this->m_ReportNewsp.end(); it++ )
-    {
-        if (sender() == ui->sendButton_address)
-        {
-            std::list <QLineEdit*> get_address = (this->getNewspAddr(it->second));
-            std::list <QLineEdit*>::iterator iter;
-            for (iter = get_address.begin(); iter != get_address.end(); iter++)
-            {
-                QHBoxLayout* hlayout = new QHBoxLayout();
-                QLabel* newsp = new QLabel();
-                QLabel* addr = new QLabel();
-                newsp->setText(QString::fromStdString(it->first));
-                addr->setText((*iter)->text());
-                hlayout->addWidget(newsp);
-                hlayout->addWidget(addr);
-                ui->vlayout_report->addLayout(hlayout);
-            }
-        } else if (sender() == ui->sendButton_support)
-        {
-            QHBoxLayout* hlayout = new QHBoxLayout();
-            QLabel* newsp = new QLabel();
-            QLabel* np_count = new QLabel();
-            newsp->setText(QString::fromStdString(it->first));
-            np_count->setText(QString::number(this->getNewspCounter(it->second))
-                              + " pcs.");
-            hlayout->addWidget(newsp);
-            hlayout->addWidget(np_count);
-            ui->vlayout_report->addLayout(hlayout);
-        }
-    }
+
+    emit sendReportData();
+}
+
+void MainWindow::showReport(QString name, QString info_value)
+{
+    QHBoxLayout* hlayout = new QHBoxLayout();
+    QLabel* newsp = new QLabel();
+    QLabel* info = new QLabel();
+    newsp->setText(name);
+    info->setText(info_value);
+    hlayout->addWidget(newsp);
+    hlayout->addWidget(info);
+    ui->vlayout_report->addLayout(hlayout);
+
     QSpacerItem* spacer = new QSpacerItem(100, 100,
                                           QSizePolicy::Expanding,
                                           QSizePolicy::Expanding);
@@ -209,60 +195,4 @@ inline QString MainWindow::getCourierAddress()
 {
     QString address = ui->lineEdit_address_courier->text();
     return address;
-}
-
-inline void MainWindow::addNewsp(std::string newsp_name, int id)
-{
-    this->m_ReportNewsp.insert(std::pair<std::string, int> (newsp_name, id));
-}
-
-inline int MainWindow::getNewspId(std::string newsp_name) const
-{
-    std::map<std::string, int>::const_iterator pos = this->m_ReportNewsp.find(newsp_name);
-    if (pos == this->m_ReportNewsp.end()) {
-        throw std::string("There is no newspaper in the list found!");
-    } else {
-        int value = pos->second;
-        return value;
-    }
-}
-
-inline void MainWindow::addNewspCounter(int id, int count)
-{
-    this->m_ReportCounters.insert(std::pair<int, int> (id, count));
-}
-
-inline int MainWindow::getNewspCounter(int id) const
-{
-    return this->m_ReportCounters.find(id)->second;
-}
-
-inline void MainWindow::addNewspAddr(int id, QLineEdit* address)
-{
-    std::map <int, std::list<QLineEdit*>>& table = this->m_ReportAddresses;
-    if ( this->m_ReportAddresses.find(id) == this->m_ReportAddresses.end())
-    {
-        std::list<QLineEdit*> addr_list;
-        addr_list.push_back(address);
-        table[id] = addr_list;
-    }
-    else
-    {
-        std::list<QLineEdit*> addr_to_push;
-        addr_to_push = table[id];
-        addr_to_push.push_back(address);
-        table[id] = addr_to_push;
-    }
-}
-
-inline std::list<QLineEdit*> MainWindow::getNewspAddr(int id) const
-{
-    return this->m_ReportAddresses.find(id)->second;
-}
-
-inline void MainWindow::clearReportData()
-{
-    this->m_ReportNewsp.clear();
-    this->m_ReportCounters.clear();
-    this->m_ReportAddresses.clear();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,12 +4,8 @@
 #include  "databasejson.h"
 
 #include <iostream>
-#include <array>
 #include <QMainWindow>
 #include <QObject>
-#include <QLabel>
-#include <QSpinBox>
-#include <iterator>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -27,9 +23,11 @@ private slots:
     void onButtonSigninClicked(QString staff_id, QString password);
     void on_button_option_proceed_clicked();
     void receiveSelectionValues(QString, int);
+    void showReport(QString, QString);
 
 signals:
     void sendSelectionValues();
+    void sendReportData();
 
 private:
     Ui::MainWindow *ui;
@@ -38,26 +36,9 @@ private:
 private:
     void moveToSelectorPage();
     void createSelectorList(const std::set<std::string>& units_list);
-    void showReport();
     void sendReport();
 
-    /* output datastructures: id, count, list(address) */
-    std::map <std::string, int> m_ReportNewsp;
-    std::map <int, int> m_ReportCounters;
-    std::map <int, std::list<QLineEdit*>> m_ReportAddresses;
-
-    inline void addNewsp(std::string newsp_name, int id);
-    inline int getNewspId(std::string newsp_name) const;
-
-    inline void addNewspCounter(int id, int count);
-    inline int getNewspCounter(int id) const;
-
-    inline void addNewspAddr(int id, QLineEdit*);
-    inline std::list<QLineEdit*> getNewspAddr(int id) const;
-
     inline QString getCourierAddress();
-
-    inline void clearReportData();
     void messageBox(std::string);
 
 private:
